cb74aba719ef9e40cfbfe361c7e770df86cf9a93
DESKTOP-P2T820I\kopo <chlqkdnf5263@gmail.com>
2023-04-18 14:17:45 +0900

    //커밋1

---
+++ helloworld.java
@@ -0,0 +1,7 @@
+public static void main(String[] args){
+    String s1 = "Hello world";
+    String s2 = "★Hello world★"
+    System.out.println(s1);
+    System.out.println(s2);
+    System.out.println();
+}
